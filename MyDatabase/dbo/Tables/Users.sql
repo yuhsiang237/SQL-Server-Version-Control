141f9359254d45fa7d16ebd71c12aaf97a0266c4
Yu Hsiang <umworksite@gmail.com>
2022-05-04 15:17:08 +0800

    Add column email to table users

--- MyDatabase/dbo/Tables/Users.sql
+++ MyDatabase/dbo/Tables/Users.sql
@@ -1,9 +1,17 @@
 ﻿CREATE TABLE [dbo].[Users] (
     [Id]        INT            IDENTITY (1, 1) NOT NULL,
     [UserName]  NVARCHAR (100) NULL,
+    [Email]  NVARCHAR (100) NULL,
     [IsValid]   BIT            NOT NULL,
     [CreatedOn] DATETIME       NOT NULL,
     [UpdatedOn] DATETIME       NOT NULL,
     CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
+
+
+
+
+GO
 
